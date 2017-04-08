24f4f7e7ade14af894262b241096e0e8ad4d9da0
Qianlong Liu <LiuQL2@sina.com>
2017-04-08 10:58:15 +0800

    update RabbitMQ.py

--- database/RabbitMQ.py
+++ database/RabbitMQ.py
@@ -15,50 +15,83 @@ class RabbitmqServer(object):
         pass
 
     @staticmethod
-    def add_message(message, routing_key, queue,queue_durable=False, exchange='',exchange_type=None,try_number=100):
+    def get_connection(try_number=100):
         success = False
         for index in range(0, try_number, 1):
+            print 'number', index
             try:
                 url = 'amqp://' + master_info['user'] + ':' + master_info['password'] + '@' + master_info[
                     'host'] + ':' + str(master_info['port'])
                 connection = pika.BlockingConnection(pika.URLParameters(url=url))
-                channel = connection.channel()
-                # 定义一个用来接收message的queue，同时为了保证消息不丢失，durable决定该queue进行持久化。
-                channel.queue_declare(queue=queue, durable=queue_durable)
-                # 定义一个exchange，用来传输message
-                if exchange != '':
-                    channel.exchange_declare(exchange=exchange, type=exchange_type)
-                    channel.queue_bind(exchange=exchange, routing_key=routing_key, queue=queue)
-                    # 将message任务发送到服务器，同时为了保证每一个在做的message任务不丢失，delivery_mode参数决定将消息进行持久化。
-                    channel.basic_publish(exchange=exchange,
-                                          routing_key=routing_key,
-                                          body=message,
-                                          properties=pika.BasicProperties(delivery_mode=2)
-                                          # make the message persistent.
-                                          )
-                    print "[x] Sent %r" % message
-                else:
-                    # 将message任务发送到服务器，同时为了保证每一个在做的message任务不丢失，delivery_mode参数决定将消息进行持久化。
-                    channel.basic_publish(exchange=exchange,
-                                          routing_key=queue,
-                                          body=message,
-                                          properties=pika.BasicProperties(delivery_mode=2)
-                                          # make the message persistent.
-                                          )
-                    print "[x] Sent %r" % message
-                connection.close()
                 success = True
-            except Exception, e:
+                return connection
+            except Exception,e:
                 print traceback.format_exc(), e.message
-                FileIO.exceptionHandler(message=traceback.format_exc() + ' ' + e.message)
-            if success:
+                FileIO.exceptionHandler(message=traceback.format_exc() + '  ' + e.message)
+            if success == True:
                 break
             else:
                 pass
         if success == False:
-            sys.exit('Rabbitmq Server Wrong.')
+            sys.exit(1)
         else:
             pass
+
+    @staticmethod
+    def add_message(message, routing_key, queue,queue_durable=False, exchange='',exchange_type=None,try_number=100):
+        try:
+            connection = RabbitmqServer.get_connection()
+            channel = connection.channel()
+            # 定义一个用来接收message的queue，同时为了保证消息不丢失，durable决定该queue进行持久化。
+            channel.queue_declare(queue=queue, durable=queue_durable)
+            # 定义一个exchange，用来传输message
+            if exchange != '':
+                channel.exchange_declare(exchange=exchange, type=exchange_type)
+                channel.queue_bind(exchange=exchange, routing_key=routing_key, queue=queue)
+                # 将message任务发送到服务器，同时为了保证每一个在做的message任务不丢失，delivery_mode参数决定将消息进行持久化。
+                channel.basic_publish(exchange=exchange,
+                                  routing_key=routing_key,
+                                  body=message,
+                                  properties=pika.BasicProperties(delivery_mode=2)
+                                  # make the message persistent.
+                                  )
+                print "[x] Sent %r" % message
+            else:
+                # 将message任务发送到服务器，同时为了保证每一个在做的message任务不丢失，delivery_mode参数决定将消息进行持久化。
+                channel.basic_publish(exchange=exchange,
+                                  routing_key=queue,
+                                  body=message,
+                                  properties=pika.BasicProperties(delivery_mode=2)
+                                  # make the message persistent.
+                                  )
+                print "[x] Sent %r" % message
+            connection.close()
+        except Exception,e:
+            print traceback.format_exc(),e.message
+            FileIO.exceptionHandler(message=traceback.format_exc() + ' ' + e.message)
+
+    @staticmethod
+    def queue_delete(queue):
+        try:
+            connection = RabbitmqServer.get_connection()
+            channel = connection.channel()
+            channel.queue_delete(queue=queue)
+            connection.close()
+        except Exception,e:
+            print traceback.format_exc(),e.message
+            FileIO.exceptionHandler(message=traceback.format_exc() + ' ' + e.message)
+
+
+    @staticmethod
+    def exchange_delete(exchange):
+        try:
+            connection = RabbitmqServer.get_connection()
+            channel = connection.channel()
+            channel.exchange_delete(exchange=exchange)
+            connection.close()
+        except Exception,e:
+            print traceback.format_exc(),e.message
+            FileIO.exceptionHandler(message=traceback.format_exc() + ' ' + e.message)
 
 
 class RabbitmqConsumer(object):
